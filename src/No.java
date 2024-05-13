a07b358563a9fc3b89a81c7bc1a412d90985062c
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-05-13 20:18:48 -0300

    No codificado.

---
+++ src/No.java
@@ -0,0 +1,35 @@
+public class No {
+    private int valor;
+    private No filhoEsquerdo;
+    private No filhoDireito;
+
+    public No(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getFilhoEsquerdo() {
+        return filhoEsquerdo;
+    }
+
+    public void setFilhoEsquerdo(No filhoEsquerdo) {
+        this.filhoEsquerdo = filhoEsquerdo;
+    }
+
+    public No getFilhoDireito() {
+        return filhoDireito;
+    }
+
+    public void setFilhoDireito(No filhoDireito) {
+        this.filhoDireito = filhoDireito;
+    }
+
+    
+}
